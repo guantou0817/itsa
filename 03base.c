cc14d270696607ae86fae99b65817404f34cd457
guantou0817 <guantou@alum.ccu.edu.tw>
2022-12-13 15:25:51 +0800

    Create 03base.c

---
+++ 03base.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    
+    int x,y;
+    
+    while(scanf("%d %d",&x,&y)!=-1){
+       
+        
+        if(x*x+y*y>10000){
+            printf("outside\n");
+        }else{
+            printf("inside\n");
+        }
+        
+    }
+    
+
+}
+        
+
+        
+            
+        
+    
+    
+    
+
+    
