b8b6d6bf2c54977b3a4fafb816ee22320045ba65
guantou0817 <guantou@alum.ccu.edu.tw>
2022-12-13 15:04:53 +0800

    Create 02base.c

---
+++ 02base.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    
+    int mile;
+    double km;
+    while((scanf("%d",&mile))==1){
+        km =1.6*mile;
+        printf("%.1lf\n",km);
+    }
+    
+
+}
+        
+
+        
+            
+        
+    
+    
+    
+
+    
